7d08336fc7d452abddcfa915a6a1a75aa67a597d
AnkySigma6969 <nanhkhoinguyen@gmail.com>
2025-02-09 15:42:30 -0500

    i hate blcks

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ display_width, display_height = game_display.get_size()
 player1 = Player(display_width * 0.2, display_height * 0.8, 30, 40, controls='wasd')
 player2 = Player(display_width * 0.7, display_height * 0.8, 30, 40, controls='arrows')
 
+player_image1 = pygame.image.load("IMAGES/Cat.jpg").convert_alpha()
+Image_Witdh=player_image1.get_width()
+Image_Height = player_image1.get_height()
+player1_image_flip = None
+
 bras_rotatif = Bras_Rotatif(0,0.01,0,0)
 bras_rotatif2 = Bras_Rotatif(0,0.01,0,0)
 
@@ -29,15 +34,13 @@ pygame.draw.rect(rect2,(0, 120, 250),(15,30,40,20))
 #Mains des personnages
 pygame.draw.rect(rect,(0,0,0),(85,30,10,20))
 pygame.draw.rect(rect2,(0,0,0),(5,30,10,20))
-
+#variables bras
 posx = player1.x_position
 posy = player1.y_position
 posx2 = player2.x_position
 posy2 = player2.y_position
 
 game_display.blit(rect,(posx,posy))
-
-
 
 player_y_Baseposition = display_height * 0.8
 
@@ -53,6 +56,8 @@ rec_centre_y=rec_taille.center[1]
 rec_taille2=rect2.get_rect()
 rec_centre_x2=rec_taille2.center[0]
 rec_centre_y2=rec_taille2.center[1]
+
+#End variables
 
 game_running = True
 while game_running:
@@ -97,13 +102,17 @@ while game_running:
     # Rendu graphique
     game_display.fill(BACKGROUND_COLOR)
 
-    player1.draw(game_display, (0, 120, 250))
+    #player1.draw(game_display, (0, 120, 250))
+    player_rect = player_image1.get_rect(center=(player1.x_position+140, player1.y_position+120))
+    imageFinal1 = pygame.transform.scale_by(player_image1, 0.3)
+    game_display.blit(imageFinal1, player_rect)
+
     player2.draw(game_display, (255, 0, 0))
 
     pygame.draw.rect(game_display, (0, 0, 0), (0, player_y_Baseposition + 40, display_width, 5))
     pygame.draw.rect(game_display, (255, 0, 0), (display_width / 2, 0, 1, display_height))
 
-
+# Physique rotation bras
     if keys[pygame.K_LSHIFT]:
         t += 0.1
         i -= bras_rotatif.Calcul_de_vitesse_angulaire(omega, t, bras_rotatif.alpha, bras_rotatif.theta)
@@ -111,6 +120,7 @@ while game_running:
         t2 += 0.1
         i2 += bras_rotatif.Calcul_de_vitesse_angulaire(omega2, t2, bras_rotatif2.alpha, bras_rotatif2.theta)
 
+# Rotation bras
     rect_rotated = pygame.transform.rotate(rect, i)
     rectangle_rot_taille = rect_rotated.get_rect()
     rectangle_rot_centre_x = rectangle_rot_taille.center[0]
@@ -125,12 +135,10 @@ while game_running:
     diff_x2 = rectangle_rot_centre_x2 - rec_centre_x2
     diff_y2 = rectangle_rot_centre_y2 - rec_centre_y2
 
-    #game_display.blit(rect_rotated, (posx - diff_x, posy - diff_y))
-    #game_display.blit(rect_rotated2, (posx2 - diff_x2, posy2 - diff_y2))
     game_display.blit(rect_rotated, (player1.x_position - diff_x -30, player1.y_position - diff_y-30))
     game_display.blit(rect_rotated2, (player2.x_position - diff_x2-40, player2.y_position - diff_y2-30))
 
-
+# Key binding
     for game_event in pygame.event.get():
         if game_event.type == pygame.QUIT:
             game_running = False
