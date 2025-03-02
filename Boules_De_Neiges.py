97eef8c1697b64b190a2d3d0f27a4fd9699fb0f3
RafaDaHacker69 <Ralfou11@gmail.com>
2025-03-02 15:04:44 -0500

    lol

--- Boules_De_Neiges.py
+++ Boules_De_Neiges.py
@@ -1,3 +1,5 @@
+import math
+
 
 class Boules_De_Neiges:
 
@@ -9,3 +11,4 @@ class Boules_De_Neiges:
 
     def lancement_projectile(self):
         print(f"Projectile lancé à : {self.theta}")
+        self.theta = self.theta * math.pi / 180 #angle en radians
