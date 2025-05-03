51df02d3d771b0f798433317d5590edc645d0ed3
RafaDaHacker69 <Ralfou11@gmail.com>
2025-05-03 19:45:30 -0400

    main

--- Loading_Screen.py
+++ Loading_Screen.py
@@ -16,10 +16,10 @@ class LoadingScreen:
         work = 100000000
 
         loading_bg = pygame.image.load('IMAGES/Loading Bar Background.png')
-        loading_rect = loading_bg.get_rect(center=(620, 340))
+        loading_rect = loading_bg.get_rect(center=(620, 440))
 
         loading_bar_original = pygame.image.load('IMAGES/Loading Bar.png')
-        loading_bar_rect = loading_bar_original.get_rect(midleft=(280, 340))
+        loading_bar_rect = loading_bar_original.get_rect(midleft=(280, 440))
 
         def doWork():
             for i in range(work):
@@ -35,14 +35,14 @@ class LoadingScreen:
                     pygame.quit()
                     sys.exit()
 
-            screen.fill((13, 14, 46))  # RGB de "#0d0e2e"
+            screen.fill((160, 200, 220))  # RGB de "#0d0e2e"
 
             if not self.loading_finished:
                 progress_ratio = self.loading_progress / work
                 loading_bar_width = max(1, int(progress_ratio * 720))
 
                 loading_bar = pygame.transform.scale(loading_bar_original, (loading_bar_width, 150))
-                loading_bar_rect = loading_bar.get_rect(midleft=(280, 340))
+                loading_bar_rect = loading_bar.get_rect(midleft=(270, 440))
 
                 screen.blit(loading_bg, loading_rect)
                 screen.blit(loading_bar, loading_bar_rect)
